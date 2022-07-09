bbd344d575d9e2e2cc9de2b448d5d7f6c654ad29
Daniela Mera <daniela.antonieta@gmail.com>
2022-07-09 03:24:18 -0400

    Devise, MVC Empresa y User (Postulantes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :empresas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,7 +8,6 @@ Rails.application.routes.draw do
   get "/parametros/genero", to:"genero#index" #/parametros/genero
   get "/parametros/tipo", to:"tipo#index" #/parametros/tipo
   get "/parametros/regionch", to:"regionch#index" #/parametros/regionch
-
   
   # root "articles#index"
   root to: "home#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_09_003134) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_09_064625) do
+  create_table "empresas", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "rut"
+    t.string "nombre"
+    t.string "rep_legal"
+    t.integer "telefono"
+    t.string "calle"
+    t.integer "numero"
+    t.string "ciudad"
+    t.integer "regionch_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_empresas_on_email", unique: true
+    t.index ["regionch_id"], name: "index_empresas_on_regionch_id"
+    t.index ["reset_password_token"], name: "index_empresas_on_reset_password_token", unique: true
+  end
+
   create_table "generos", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -27,6 +48,27 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_09_003134) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "rut"
+    t.string "nombre"
+    t.string "ap_paterno"
+    t.string "ap_materno"
+    t.date "nacimiento"
+    t.integer "genero_id"
+    t.integer "telefono"
+    t.string "cv"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["genero_id"], name: "index_users_on_genero_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
